5c56a0c30c6a04c1bc171a5b07ffd0e61069e2e2
Ling Ding <35885425+azusakaworu@users.noreply.github.com>
2018-10-24 12:02:36 -0400

    updats.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
 (() => {
+    
+    //grab the car buttons
+    const cars = document.querrySelectorALL('.data-ref');
     function fetchData(){
-      fetch('./includes/Pcooper.php?carModel=R58')
+      fetch(`./includes/Pcooper.php?carModel=$(this.id)`)
     .then (res = res.json())
-    .then (data => console.log(data))
+    .then (data => {
+          
+          console.log(data);
+          parseCarData(data[0]);
+      })
     .catch(function(error){
         console.error(error);
     });
     }
 
-
+function parseCarData(Car){
+  
+    //destreucture the database info and grab just what we need
+    const {modelName, pricing, modelDetails }= car
+    //take the database data and put it on the page
+    document.querySelector(".modelName")=textContent =modelName;
+        document.querySelector(".priceInfo")=textContent = pricing;
+        document.querySelector(".modelDetails")=textContent =modelDetails;
+};
+    
+    cars.forEach (car=> car.addEventlistener("click,.....)
   fetchDta();
 
 })();
